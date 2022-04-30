6f5401a332ce2fd2ab17627a9105883025fe80d9
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-04-30 18:18:57 +0400

    about page beta finished

--- src/components/about/AboutBanner.jsx
+++ src/components/about/AboutBanner.jsx
@@ -8,7 +8,7 @@ export const AboutBanner = () => {
     <div>
         <div className="container">
             <div className="row row-marginer">
-            <h1 class="about-title">About Us</h1>
+            <h1 class="about-title">ABOUT US</h1>
             </div>
             <div className="row row-marginer">
                 <div className="col-6 about--img">
@@ -23,18 +23,18 @@ export const AboutBanner = () => {
             <div className="row row-marginer">
 
                     <div className="col-6 about--text__left">
-                        <h3>
+                        <h3 className='about--text__hmod'>
                         In 2020 Apart Development
                         </h3>
-                        <p>
+                        <p className='about--text__ptmod2'>
                         started new project OLD CITY PANORAMA whitch is different and special poject in Tbilisi. Project perfectly suits the architectural style of Old Tbilisi, It is located on Bukhaidze Street, in the historical part of Tbilisi. Our goal is to match the exterior of the project perfectly with the wider city planning, which is why the Old City Panorama project includes three buildings and is only eight floors high. The project is also important because it has many sights close to it: one of the most important attractions of the city, the Holy Trinity Cathedral, just 250 meters away, the historic Metekhi Fortress and Temple, and Narikala and Rike Park.
                         </p>
                     </div>
                     <div className="col-6 about--text__right">
-                        <h3>
+                        <h3  className='about--text__hmod'>
                         In 2020 Apart Development
                         </h3>
-                        <p>
+                        <p className='about--text__ptmod__mod'>
                         started new project OLD CITY PANORAMA whitch is different and special poject in Tbilisi. Project perfectly suits the architectural style of Old Tbilisi, It is located on Bukhaidze Street, in the historical part of Tbilisi. Our goal is to match the exterior of the project perfectly with the wider city planning, which is why the Old City Panorama project includes three buildings and is only eight floors high. The project is also important because it has many sights close to it: one of the most important attractions of the city, the Holy Trinity Cathedral, just 250 meters away, the historic Metekhi Fortress and Temple, and Narikala and Rike Park.
                         </p>
                     </div>
@@ -42,6 +42,145 @@ export const AboutBanner = () => {
                 
             </div>
 
+            <div className="row justify-content-between">
+                    <div className="col-4">
+                        <hr />
+                    </div>
+                    <div className="col-4">
+                        <hr />
+                    </div>
+            </div>
+
+        </div>
+
+
+        <div className="container">
+            <div className="row row-marginer">
+                <h1 class="about-title">OUR TEAM</h1>
+            </div>
+            <div className="row row-marginer">
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+            </div>
+
+            <div className="row row-marginer">
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+            </div>
+
+            <div className="row row-marginer">
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+            </div>
+
+            <div className="row row-marginer">
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+                <div className="col-3">
+                <Image src={BannerImg} alt="banner immage" /> 
+                <h3>
+                Beka Khoperia
+                </h3>
+                <p>Founder</p>
+                </div>
+            </div>
         </div>
 
 
