7e8f364eb27832ec82bbffddb75a3af7f3c3bbb0
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-04-30 15:45:05 +0400

    footer beautify update

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,105 +3,100 @@ import { Nav, Button,Row,Container,input } from 'react-bootstrap'
 import Image from 'next/image'
 import Img from '../../../assets/footer-logo.svg'
 function footer() {
-  return (
-    <div className='footer-outside'>
-      <Container fluid className='footer-container'>
-        <Row>
-          <div className='col-lg-3'>
-
-          </div>
-
-          <div className='col-lg-6 footer-logo'>
-          <Image
-            src={Img}
-            alt="Logo"
-          />
-          </div>
-
-          <div className='col-lg-3'>
+return (
+<div className='footer-outside'>
+   <Container fluid className='footer-container'>
+      <Row>
+         <div className='col-lg-3'>
+         </div>
+         <div className='col-lg-6 footer-logo'>
+            <Image
+               src={Img}
+               alt="Logo"
+               />
+         </div>
+         <div className='col-lg-3'>
             <ul className='languages'>
-              <li>
-                <a href='#'><p>GEO /</p></a>
-              </li>
-              <li>
-              <a href='#'><p> <span className='active'>ENG</span> /</p></a>
-              </li>
-              <li>
-              <a href='#'><p>RUS /</p></a>
-              </li>
+               <li>
+                  <a href='#'>
+                     <p>GEO /</p>
+                  </a>
+               </li>
+               <li>
+                  <a href='#'>
+                     <p> <span className='active'>ENG</span> /</p>
+                  </a>
+               </li>
+               <li>
+                  <a href='#'>
+                     <p>RUS /</p>
+                  </a>
+               </li>
             </ul>
-          </div>
-        </Row>
-        <Row className='footer-info'>
-          <div className='col-lg-3'>
-              <ul className='footer-info-ul'>
-                <p className='footer-info-title'>
+         </div>
+      </Row>
+      <Row className='footer-info'>
+         <div className='col-lg-3'>
+            <ul className='footer-info-ul'>
+               <p className='footer-info-title'>
                   PRODUCTS
-                </p>
-                <li >
+               </p>
+               <li >
                   UPDATES
-                </li>
-                <li>
+               </li>
+               <li>
                   SECURITY
-                </li>
-                <li>
+               </li>
+               <li>
                   EXTENSION
-                </li>
-              </ul>
-          </div>
-
-          <div className='col-lg-3'>
-              <ul className='footer-info-ul'>
-                <p className='footer-info-title'>
+               </li>
+            </ul>
+         </div>
+         <div className='col-lg-3'>
+            <ul className='footer-info-ul'>
+               <p className='footer-info-title'>
                   COMPANY
-                </p>
-                <li>
+               </p>
+               <li>
                   ABOUT
-                </li>
-                <li>
+               </li>
+               <li>
                   BLOG
-                </li>
-                <li>
+               </li>
+               <li>
                   JOIN US
-                </li>
-              </ul>
-          </div>
-
-          <div className='col-lg-3'>
-              <ul className='footer-info-ul'>
-                <p className='footer-info-title'>
+               </li>
+            </ul>
+         </div>
+         <div className='col-lg-3'>
+            <ul className='footer-info-ul'>
+               <p className='footer-info-title'>
                   INDUSTRIES
-                </p>
-                <li>
+               </p>
+               <li>
                   STARTUPS
-                </li>
-                <li>
+               </li>
+               <li>
                   CAPITAL
-                </li>
-                <li>
+               </li>
+               <li>
                   EQUILITY
-                </li>
-              </ul>
-          </div>
-        </Row>
-        <Row>
-          <div className='footer-terms'>
+               </li>
+            </ul>
+         </div>
+      </Row>
+      <Row>
+         <div className='footer-terms'>
             <p className='footer--terms__mod'>
-          All rights reserved. The copyright notice generally consists of three elements
-          </p>
-          <a><p className='footer--terms__terms__mod'>Terms and Conditions</p></a>
-          </div>
-      
-        </Row>
-      </Container>
-
-
-
-
-
-
-    </div>
-  )
+               All rights reserved. The copyright notice generally consists of three elements
+            </p>
+            <a>
+               <p className='footer--terms__terms__mod'>Terms and Conditions</p>
+            </a>
+         </div>
+      </Row>
+   </Container>
+</div>
+)
 }
-
 export default footer
