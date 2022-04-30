d5faf921ef538d7fa436ee28a57e90795cd2c43c
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-04-30 17:41:02 +0400

    about page 200vh

--- src/components/about/AboutBanner.jsx
+++ src/components/about/AboutBanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Nav, Button,Row,Container,input } from 'react-bootstrap'
 import BannerImg from '../../../assets/aboutbannerimg.svg'
+import Image from 'next/image'
 
 export const AboutBanner = () => {
   return (
@@ -10,14 +11,36 @@ export const AboutBanner = () => {
             <h1 class="about-title">About Us</h1>
             </div>
             <div className="row row-marginer">
-                <div className="aboutImg">
-                    <img src={BannerImg} alt="" />
+                <div className="col-6 about--img">
+                    <Image src={BannerImg} alt="banner immage" />
                 </div>
-                <div className="about-text">
-
+                <div className="col-6 about--text">
+                    <h3 className='about--text__hmod'>About Apart Development</h3>
+                    <p className='about--text__ptmod'>The company Apart Development was founded on April 15, 2019,</p>
+                    <p className='about--text__ptmod2'>although we are a growing construction company and we have an ongoing, innovative project. We build construction in accordance with the highest standards, which means that our construction sites are distinguished with constructive and seismic sustainability. Each current or future project of the company is distinguished with sophisticated exterior, effective internal planning, and most importantly with the best location. The aim of the company is to create a product that will be tailored to the user's needs and wishes. We would like to have your lucrative investment in the company acquired in the company. What makes us different on the market is: • A high quality that can be found in each detail; • Innovation, which is a successful business success; • Responsibility, without which can not gain customer trust</p>
                 </div>
             </div>
+            <div className="row row-marginer">
 
+                    <div className="col-6 about--text__left">
+                        <h3>
+                        In 2020 Apart Development
+                        </h3>
+                        <p>
+                        started new project OLD CITY PANORAMA whitch is different and special poject in Tbilisi. Project perfectly suits the architectural style of Old Tbilisi, It is located on Bukhaidze Street, in the historical part of Tbilisi. Our goal is to match the exterior of the project perfectly with the wider city planning, which is why the Old City Panorama project includes three buildings and is only eight floors high. The project is also important because it has many sights close to it: one of the most important attractions of the city, the Holy Trinity Cathedral, just 250 meters away, the historic Metekhi Fortress and Temple, and Narikala and Rike Park.
+                        </p>
+                    </div>
+                    <div className="col-6 about--text__right">
+                        <h3>
+                        In 2020 Apart Development
+                        </h3>
+                        <p>
+                        started new project OLD CITY PANORAMA whitch is different and special poject in Tbilisi. Project perfectly suits the architectural style of Old Tbilisi, It is located on Bukhaidze Street, in the historical part of Tbilisi. Our goal is to match the exterior of the project perfectly with the wider city planning, which is why the Old City Panorama project includes three buildings and is only eight floors high. The project is also important because it has many sights close to it: one of the most important attractions of the city, the Holy Trinity Cathedral, just 250 meters away, the historic Metekhi Fortress and Temple, and Narikala and Rike Park.
+                        </p>
+                    </div>
+
+                
+            </div>
 
         </div>
 
