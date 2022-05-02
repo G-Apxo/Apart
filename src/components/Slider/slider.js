9e471878fd1f1114f6596fb6656ff0e048609f29
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-05-02 15:47:39 +0400

    cretion of the contact form

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -5,7 +5,6 @@ function slider() {
   return (
         <Container>
             <Row>
-           
             <iframe src='https://my.spline.design/cubeaxiswebsiteupdatedinprod-c7eb2ea95c5e22c50b14b5333ee86583/' frameborder='0' width='100%' height='100%'></iframe>
             </Row>
         </Container>
