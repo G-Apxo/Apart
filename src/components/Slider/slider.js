8a93944cea07d0164d932bd8591c97938577d0e8
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-05-02 16:40:10 +0400

    slider layout design creation

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -1,41 +1,70 @@
 import React from 'react'
-import { Nav, Button,Row,Container,input,Carousel } from 'react-bootstrap'
+import { Nav, Button,Row,Container,input,Carousel, Col } from 'react-bootstrap'
 import Slide1 from '../../../assets/slider.png'
+import Taghi from '../../../assets/taghi.svg'
 import Image from 'next/image'
 
 function slider() {
   return (
-        <Container>
-            <Row>
             <Carousel>
               <Carousel.Item>
-              <Image src={Slide1} alt="banner immage" />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                <Row>
+                  <Col className="col-7 justify-content-center d-flex">
+                  <Image src={Slide1} alt="banner immage" />
+                  </Col>
+                  <Col className='col-5 d-flex align-items-center'>
+                    <div className=''>
+                    <Image src={Taghi} alt="banner immage" />
+                    <h3>OLD TBILISI</h3>
+                    <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
+                    <br/>
+                    <br/>
+                    For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
+                    </p>
+                    </div>
+                
+                  </Col>
+                </Row>
               </Carousel.Item>
               <Carousel.Item>
-              <Image src={Slide1} alt="banner immage" />
-
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+              <Row>
+                  <Col className="col-7 justify-content-center d-flex">
+                  <Image src={Slide1} alt="banner immage" />
+                  </Col>
+                  <Col className='col-5 d-flex align-items-center'>
+                    <div className=''>
+                    <Image src={Taghi} alt="banner immage" />
+                    <h3>OLD TBILISI</h3>
+                    <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
+                    <br/>
+                    <br/>
+                    For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
+                    </p>
+                    </div>
+                
+                  </Col>
+                </Row>
               </Carousel.Item>
               <Carousel.Item>
-              <Image src={Slide1} alt="banner immage" />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
+              <Row>
+                  <Col className="col-7 justify-content-center d-flex">
+                  <Image src={Slide1} alt="banner immage" />
+                  </Col>
+                  <Col className='col-5 d-flex align-items-center'>
+                    <div className=''>
+                    <Image src={Taghi} alt="banner immage" />
+                    <h3>OLD TBILISI</h3>
+                    <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
+                    <br/>
+                    <br/>
+                    For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
+                    </p>
+                    </div>
+                
+                  </Col>
+                </Row>
               </Carousel.Item>
             </Carousel>
-          
-            </Row>
-        </Container>
-
   )
 }
 
