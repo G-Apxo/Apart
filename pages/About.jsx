18b7d6888f23dc944e6a53359eca3264eca03a2e
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-05-03 13:33:38 +0400

    l

--- pages/About.jsx
+++ pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
       <Head>
         <title>About page</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap"
           rel="stylesheet"
