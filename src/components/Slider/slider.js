a64b9ad92971643f306bf1936466a99f4c4c356c
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-05-16 14:32:10 +0400

    apart sldier update at least we are close

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -1,117 +1,76 @@
-import React, { Component } from 'react'
+import { useState } from "react";
+import Slider from "react-slick";
+import astronaut from "../../assets/astronaut.png";
+import celebrating from "../../assets/celebrating.png";
+import education from "../../assets/education.png";
+import taken from "../../assets/taken.png";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import { Nav, Button,Row,Container,input,Carousel, Col } from 'react-bootstrap'
 import Slide1 from '../../../assets/slider.png'
 import Taghi from '../../../assets/taghi.svg'
 
 import Image from 'next/image'
 
-
+const images = [Slide1, celebrating, education, taken];
 function slider() {
-  return (
-    <Container>
-      <Row className='justify-content-between'>
-          <Col xs="2">
-            <ul className='custom__li'>
-              <li>
-                Apart
-              </li>
-              <li>
-              Old city panorama
-              </li>
-              <li>
-                Apart
-              </li>
-              <li>
-                Apart
-              </li>
-              <li>
-                Apart
-              </li>
-            </ul>
-          </Col>
-        <Col xs="5">
-        <Carousel>
-              <Carousel.Item>
-                <Row>
-                  <Col className='col-12 d-flex align-items-center justify-content-end'>
-                  <Image src={Slide1} alt="banner immage" />
-                  </Col>
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item>
-              <Row>
-                  <Col className='col-12 d-flex align-items-center justify-content-end'>
-                  <Image src={Slide1} alt="banner immage" />
-                
-                  </Col>
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item>
-              <Row>
-                  <Col className='col-12 d-flex align-items-center justify-content-end'>
-                  <Image src={Slide1} alt="banner immage" />
-                
-                  </Col>
-                </Row>
-              </Carousel.Item>
-            </Carousel>
-        </Col>
-        <Col xs="5" className='d-flex justify-content-start align-items-center'>
-        <Carousel>
-              <Carousel.Item>
-                <Row>
-                  <Col className='col-12 d-flex align-items-center'>
-                    <div className=''>
-                    <Image src={Taghi} alt="banner immage" />
-                    <h3>OLD TBILISI</h3>
-                    <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
-                    <br/>
-                    <br/>
-                    For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
-                    </p>
-                    </div>
-                
-                  </Col>
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item>
-              <Row>
-                  <Col className='col-12 d-flex align-items-center'>
-                    <div className=''>
-                    <Image src={Taghi} alt="banner immage" />
-                    <h3>OLD TBILISI</h3>
-                    <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
-                    <br/>
-                    <br/>
-                    For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
-                    </p>
-                    </div>
-                
-                  </Col>
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item>
-              <Row>
-                  <Col className='col-12 d-flex align-items-center'>
-                    <div className=''>
-                    <Image src={Taghi} alt="banner immage" />
-                    <h3>OLD TBILISI</h3>
-                    <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
-                    <br/>
-                    <br/>
-                    For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
-                    </p>
-                    </div>
-                
-                  </Col>
-                </Row>
-              </Carousel.Item>
-            </Carousel>
-        </Col>
-      </Row>
-      </Container>
-  
-  )
-}
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    );
+  };
 
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+
+  return (
+    <div className="App">
+      <Slider {...settings}>
+        {images.map((img, idx) => (
+          <div style={{ width: 100 }} className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+            <Row className="justify-content-between">
+              <Col xs="7">
+              <Image src={Slide1} alt="banner immage" />
+              </Col>
+              <Col xs="3" className="d-flex">
+              <Col className='col-12 d-flex align-items-center'>
+                <div className=''>
+                <Image src={Taghi} alt="banner immage" />
+                <h3>OLD TBILISI</h3>
+                <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
+                <br/>
+                <br/>
+                For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
+                </p>
+                </div>
+            
+              </Col>
+              </Col>
+            </Row>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
 export default slider
