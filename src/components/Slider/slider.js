9aad564300c0bc3237dc8737011b0a38fa1912d9
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-05-17 21:53:02 +0400

    test

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState,Suspense } from "react";
 import Slider from "react-slick";
-import astronaut from "../../assets/astronaut.png";
 import celebrating from "../../assets/celebrating.png";
 import education from "../../assets/education.png";
 import taken from "../../assets/taken.png";
+import { Canvas } from '@react-three/fiber'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import { Nav, Button,Row,Container,input,Carousel, Col } from 'react-bootstrap'
 import Slide1 from '../../../assets/slider.png'
 import Taghi from '../../../assets/taghi.svg'
 
 import Image from 'next/image'
-
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./scene.gltf")
+  return (
+      <>
+          <primitive  position={[0, 0, 0]} object={gltf.scene} scale={1} />
+      </>
+  );
+};
 const images = [Slide1, celebrating, education, taken];
 function slider() {
   const NextArrow = ({ onClick }) => {
