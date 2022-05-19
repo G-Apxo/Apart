ee309c1d69946f500df21e157c19e378c4f3ec87
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-05-19 15:59:23 +0400

    new component creation

---
+++ src/components/Projectsmap/Projectsmap.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Nav , Button, Row,Container,Col, Carousel, Form,FormGroup, Label, Input, FormText} from 'react-bootstrap'
+const Projectsmap = () => {
+  return (
+    <div>
+        <Container>
+            <Row>
+              
+            </Row>
+          </Container>
+    </div>
+  )
+}
+export default Projectsmap;
