835c822825af6dc54ed7cfe1890a8d120c64b2a7
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-05-19 19:37:28 +0400

    node update on swc darwin

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -1,24 +1,24 @@
-import { useState,Suspense } from "react";
+import { useState, Suspense } from "react";
 import Slider from "react-slick";
 import celebrating from "../../assets/celebrating.png";
 import education from "../../assets/education.png";
 import taken from "../../assets/taken.png";
-import { Canvas } from '@react-three/fiber'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
-import { Nav, Button,Row,Container,input,Carousel, Col } from 'react-bootstrap'
-import Slide1 from '../../../assets/slider.png'
-import Taghi from '../../../assets/taghi.svg'
+import { Nav, Button, Row, Container, input, Carousel, Col } from "react-bootstrap";
+import Slide1 from "../../../assets/slider.png";
+import Taghi from "../../../assets/taghi.svg";
 
-import Image from 'next/image'
+import Image from "next/image";
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./scene.gltf")
+  const gltf = useLoader(GLTFLoader, "./scene.gltf");
   return (
-      <>
-          <primitive  position={[0, 0, 0]} object={gltf.scene} scale={1} />
-      </>
+    <>
+      <primitive position={[0, 0, 0]} object={gltf.scene} scale={1} />
+    </>
   );
 };
 const images = [Slide1, celebrating, education, taken];
@@ -44,7 +44,7 @@ function slider() {
   const settings = {
     infinite: true,
     lazyLoad: true,
-    speed: 300,
+    speed: 450,
     slidesToShow: 3,
     centerMode: true,
     centerPadding: 0,
@@ -55,34 +55,36 @@ function slider() {
 
   return (
     <div className="custom_row">
-    <div className="App">
-      <Slider {...settings}>
-        {images.map((img, idx) => (
-          <div style={{ width: 100 }} className={idx === imageIndex ? "slide activeSlide" : "slide"}>
-            <Row className="justify-content-between">
-              <Col xs="7">
-              <Image src={Slide1} alt="banner immage" />
-              </Col>
-              <Col xs="4" className="d-flex">
-              <Col className='col-12 d-flex align-items-center'>
-                <div className='test'>
-                <Image src={Taghi} alt="banner immage" />
-                <h3>OLD TBILISI</h3>
-                <p>For our team, it is important and a priority to preserve   <br/>the charm of the old city, to use the old
-                <br/>
-                <br/>
-                For our team, it is important and a priority to preserve  <br/> the charm of the old city, to use the old
-                </p>
-                </div>
-            
-              </Col>
-              </Col>
-            </Row>
-          </div>
-        ))}
-      </Slider>
-    </div>
+      <div className="App">
+        <Slider {...settings}>
+          {images.map((img, idx) => (
+            <div style={{ width: 100 }} className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+              <Row className="justify-content-between">
+                <Col xs="7">
+                  <Image src={Slide1} alt="banner immage" />
+                </Col>
+                <Col xs="4" className="d-flex">
+                  <Col className="col-12 d-flex align-items-center">
+                    <div className="test">
+                      <Image src={Taghi} alt="banner immage" />
+                      <h3>OLD TBILISI</h3>
+                      <p>
+                        For our team, it is important and a priority to preserve <br />
+                        the charm of the old city, to use the old
+                        <br />
+                        <br />
+                        For our team, it is important and a priority to preserve <br /> the charm of the old
+                        city, to use the old
+                      </p>
+                    </div>
+                  </Col>
+                </Col>
+              </Row>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 }
-export default slider
+export default slider;
