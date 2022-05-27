db9ef8de8425891f39604d391c411bdde5771d08
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-05-28 03:58:42 +0400

    projects page completed

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -15,6 +15,7 @@ import {
 import Layout from "../src/components/Layout/Layout";
 import Slide1 from "../assets/oldcitypanorama.png";
 import Vector from "../assets/seeprjvector.png";
+import Bukh from "../assets/apart assets/projects/Frame154.png";
 
 import Image from "next/image";
 const Projects = () => {
@@ -28,22 +29,20 @@ const Projects = () => {
       <Container className="container-borderer">
         <Row>
           <Col xs="3">
-            <div className="mb-5">
-              <div>
-                <p>Done</p>
-                <p>Bukhaidze</p>
-              </div>
+            <div className="mb-5 projects--title">
+              <p>Done</p>
+
               <h3>
                 OLD CITY <br /> PANORAMA
               </h3>
             </div>
-            <div className="mt-5">
-              <p>Location</p>
-              <h3 className="project--location__mod">Tbilisi,georgia,bukhaidze 6a</h3>
+            <div className="projects--legend__top projects--texts__basics">
+              <p className="projects--texts__op">Location</p>
+              <h3 className="project--location__mod projects--texts__basics">Tbilisi,georgia,bukhaidze 6a</h3>
             </div>
-            <div className="mt-5">
-              <p>Create date</p>
-              <h3>2022</h3>
+            <div className="projects--legend__bot">
+              <p className="projects--texts__op">Create date</p>
+              <h3 className="projects--texts__basics">2022</h3>
             </div>
             <div>
               <i className="project--see">
@@ -73,22 +72,18 @@ const Projects = () => {
       <Container className="container-borderer">
         <Row>
           <Col xs="3">
-            <div className="mb-5">
-              <div>
-                <p>Done</p>
-                <p>Bukhaidze</p>
-              </div>
-              <h3>
-                OLD CITY <br /> PANORAMA
-              </h3>
+            <div className="mb-5 projects--title">
+              <p>Done</p>
+
+              <h3>Bukhaidze</h3>
             </div>
-            <div className="mt-5">
-              <p>Location</p>
-              <h3 className="project--location__mod">Tbilisi,georgia,bukhaidze 6a</h3>
+            <div className="projects--legend__top projects--texts__basics">
+              <p className="projects--texts__op">Location</p>
+              <h3 className="project--location__mod projects--texts__basics">Tbilisi,georgia,bukhaidze 6a</h3>
             </div>
-            <div className="mt-5">
-              <p>Create date</p>
-              <h3>2022</h3>
+            <div className="projects--legend__bot">
+              <p className="projects--texts__op">Create date</p>
+              <h3 className="projects--texts__basics">2022</h3>
             </div>
             <div>
               <i className="project--see">
@@ -99,9 +94,50 @@ const Projects = () => {
           <Col xs="9">
             <Carousel style={{ opacity: 1 }}>
               <Carousel.Item className="fuul__width">
+                <Image src={Bukh} alt="banner immage" />
+              </Carousel.Item>
+              <Carousel.Item className="fuul__width">
                 <Row>
-                  <Image src={Slide1} alt="banner immage" />
+                  <Image src={Bukh} alt="banner immage" />
                 </Row>
+              </Carousel.Item>
+              <Carousel.Item className="fuul__width">
+                <Row>
+                  <Image src={Bukh} alt="banner immage" />
+                </Row>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="container-borderer">
+        <Row>
+          <Col xs="3">
+            <div className="mb-5 projects--title">
+              <p>Done</p>
+
+              <h3>
+                OLD CITY <br /> PANORAMA
+              </h3>
+            </div>
+            <div className="projects--legend__top projects--texts__basics">
+              <p className="projects--texts__op">Location</p>
+              <h3 className="project--location__mod projects--texts__basics">Tbilisi,georgia,bukhaidze 6a</h3>
+            </div>
+            <div className="projects--legend__bot">
+              <p className="projects--texts__op">Create date</p>
+              <h3 className="projects--texts__basics">2022</h3>
+            </div>
+            <div>
+              <i className="project--see">
+                See Projects <Image src={Vector} />
+              </i>
+            </div>
+          </Col>
+          <Col xs="9">
+            <Carousel style={{ opacity: 1 }}>
+              <Carousel.Item className="fuul__width">
+                <Image src={Slide1} alt="banner immage" />
               </Carousel.Item>
               <Carousel.Item className="fuul__width">
                 <Row>
@@ -120,22 +156,20 @@ const Projects = () => {
       <Container className="container-borderer">
         <Row>
           <Col xs="3">
-            <div className="mb-5">
-              <div>
-                <p>Done</p>
-                <p>Bukhaidze</p>
-              </div>
+            <div className="mb-5 projects--title">
+              <p>Done</p>
+
               <h3>
                 OLD CITY <br /> PANORAMA
               </h3>
             </div>
-            <div className="mt-5">
-              <p>Location</p>
-              <h3 className="project--location__mod">Tbilisi,georgia,bukhaidze 6a</h3>
+            <div className="projects--legend__top projects--texts__basics">
+              <p className="projects--texts__op">Location</p>
+              <h3 className="project--location__mod projects--texts__basics">Tbilisi,georgia,bukhaidze 6a</h3>
             </div>
-            <div className="mt-5">
-              <p>Create date</p>
-              <h3>2022</h3>
+            <div className="projects--legend__bot">
+              <p className="projects--texts__op">Create date</p>
+              <h3 className="projects--texts__basics">2022</h3>
             </div>
             <div>
               <i className="project--see">
@@ -146,56 +180,7 @@ const Projects = () => {
           <Col xs="9">
             <Carousel style={{ opacity: 1 }}>
               <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="container-borderer">
-        <Row>
-          <Col xs="3">
-            <div className="mb-5">
-              <div>
-                <p>Done</p>
-                <p>Bukhaidze</p>
-              </div>
-              <h3>
-                OLD CITY <br /> PANORAMA
-              </h3>
-            </div>
-            <div className="mt-5">
-              <p>Location</p>
-              <h3 className="project--location__mod">Tbilisi,georgia,bukhaidze 6a</h3>
-            </div>
-            <div className="mt-5">
-              <p>Create date</p>
-              <h3>2022</h3>
-            </div>
-            <div>
-              <i className="project--see">
-                See Projects <Image src={Vector} />
-              </i>
-            </div>
-          </Col>
-          <Col xs="9">
-            <Carousel style={{ opacity: 1 }}>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
+                <Image src={Slide1} alt="banner immage" />
               </Carousel.Item>
               <Carousel.Item className="fuul__width">
                 <Row>
