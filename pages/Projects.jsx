fadc9763e347c22952f379c5e5b01877b8d10f01
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-06-06 04:13:07 +0400

    late night update

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -52,7 +52,7 @@ const Projects = () => {
                 </div>
                 <div className="projects--legend__bot">
                   <p className="projects--texts__op projects--texts__create">Create date</p>
-                  <h3 className="projects--texts__basics ">2022</h3>
+                  <h3 className="projects--texts__basics projects--texts__done ">2022</h3>
                 </div>
                 <div>
                   <Link href="/ProjectsFloor">
@@ -83,143 +83,168 @@ const Projects = () => {
           </Container>
         </div>
       </div>
-      <Container className="container-borderer">
-        <Row>
-          <Col xs="3">
-            <div className="mb-5 projects--title">
-              <p>Done</p>
-
-              <h3>Bukhaidze</h3>
-            </div>
-            <div className="projects--legend__top projects--texts__basics">
-              <p className="projects--texts__op">Location</p>
-              <h3 className="project--location__mod projects--texts__basics">
-                Tbilisi,georgia
-                <br />
-                bukhaidze 6a
-              </h3>
-            </div>
-            <div className="projects--legend__bot">
-              <p className="projects--texts__op">Create date</p>
-              <h3 className="projects--texts__basics">2022</h3>
-            </div>
-            <div>
-              <i className="project--see">
-                See Projects <Image src={Vector} />
-              </i>
-            </div>
-          </Col>
-          <Col xs="9">
-            <Carousel style={{ opacity: 1 }}>
-              <Carousel.Item className="fuul__width">
-                <Image src={Bukh} alt="banner immage" />
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Bukh} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Bukh} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="container-borderer">
-        <Row>
-          <Col xs="3">
-            <div className="mb-5 projects--title">
-              <p>Done</p>
-
-              <h3>
-                OLD CITY <br /> PANORAMA
-              </h3>
-            </div>
-            <div className="projects--legend__top projects--texts__basics">
-              <p className="projects--texts__op">Location</p>
-              <h3 className="project--location__mod projects--texts__basics">
-                Tbilisi,georgia<br></br>bukhaidze 6a
-              </h3>
-            </div>
-            <div className="projects--legend__bot">
-              <p className="projects--texts__op">Create date</p>
-              <h3 className="projects--texts__basics">2022</h3>
-            </div>
-            <div>
-              <i className="project--see">
-                See Projects <Image src={Vector} />
-              </i>
-            </div>
-          </Col>
-          <Col xs="9">
-            <Carousel style={{ opacity: 1 }}>
-              <Carousel.Item className="fuul__width">
-                <Image src={Slide1} alt="banner immage" />
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="container-borderer">
-        <Row>
-          <Col xs="3">
-            <div className="mb-5 projects--title">
-              <p>Done</p>
-
-              <h3>
-                OLD CITY <br /> PANORAMA
-              </h3>
-            </div>
-            <div className="projects--legend__top projects--texts__basics">
-              <p className="projects--texts__op">Location</p>
-              <h3 className="project--location__mod projects--texts__basics">
-                Tbilisi,georgia
-                <br />
-                bukhaidze 6a
-              </h3>
-            </div>
-            <div className="projects--legend__bot">
-              <p className="projects--texts__op">Create date</p>
-              <h3 className="projects--texts__basics">2022</h3>
-            </div>
-            <div>
-              <i className="project--see">
-                See Projects <Image src={Vector} />
-              </i>
-            </div>
-          </Col>
-          <Col xs="9">
-            <Carousel style={{ opacity: 1 }}>
-              <Carousel.Item className="fuul__width">
-                <Image src={Slide1} alt="banner immage" />
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item className="fuul__width">
-                <Row>
-                  <Image src={Slide1} alt="banner immage" />
-                </Row>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
+      <div className="parent1313">
+        <div className="test1313">
+          <Container className="container-borderer">
+            <Row>
+              <Col xs="3">
+                <Link href="/ProjectsFloor">
+                  <div className="mb-5 projects--title cursor">
+                    <p>Done</p>
+                    <h3>
+                      OLD CITY <br /> PANORAMA
+                    </h3>
+                  </div>
+                </Link>
+                <div className="projects--legend__top projects--texts__basics">
+                  <p className="projects--texts__op">Location</p>
+                  <h3 className="project--location__mod projects--texts__basics">
+                    Tbilisi,georgia
+                    <br />
+                    bukhaidze 6a
+                  </h3>
+                </div>
+                <div className="projects--legend__bot">
+                  <p className="projects--texts__op projects--texts__create">Create date</p>
+                  <h3 className="projects--texts__basics projects--texts__done ">2022</h3>
+                </div>
+                <div>
+                  <Link href="/ProjectsFloor">
+                    <i className="project--see cursor">
+                      View Projects <Image src={Vector} />
+                    </i>
+                  </Link>
+                </div>
+              </Col>
+              <Col xs="9" className="projects-margin-fixer">
+                <Carousel style={{ opacity: 1 }}>
+                  <Carousel.Item className="fuul__width">
+                    <Image src={Slide1} alt="banner immage" className="full_width" />
+                  </Carousel.Item>
+                  <Carousel.Item className="fuul__width">
+                    <Row>
+                      <Image src={Slide1} alt="banner immage " className="full_width" />
+                    </Row>
+                  </Carousel.Item>
+                  <Carousel.Item className="fuul__width">
+                    <Row>
+                      <Image src={Slide1} alt="banner immage" className="full_width" />
+                    </Row>
+                  </Carousel.Item>
+                </Carousel>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+      <div className="parent1313">
+        <div className="test1313">
+          <Container className="container-borderer">
+            <Row>
+              <Col xs="3">
+                <Link href="/ProjectsFloor">
+                  <div className="mb-5 projects--title cursor">
+                    <p>Done</p>
+                    <h3>
+                      OLD CITY <br /> PANORAMA
+                    </h3>
+                  </div>
+                </Link>
+                <div className="projects--legend__top projects--texts__basics">
+                  <p className="projects--texts__op">Location</p>
+                  <h3 className="project--location__mod projects--texts__basics">
+                    Tbilisi,georgia
+                    <br />
+                    bukhaidze 6a
+                  </h3>
+                </div>
+                <div className="projects--legend__bot">
+                  <p className="projects--texts__op projects--texts__create">Create date</p>
+                  <h3 className="projects--texts__basics projects--texts__done ">2022</h3>
+                </div>
+                <div>
+                  <Link href="/ProjectsFloor">
+                    <i className="project--see cursor">
+                      View Projects <Image src={Vector} />
+                    </i>
+                  </Link>
+                </div>
+              </Col>
+              <Col xs="9" className="projects-margin-fixer">
+                <Carousel style={{ opacity: 1 }}>
+                  <Carousel.Item className="fuul__width">
+                    <Image src={Slide1} alt="banner immage" className="full_width" />
+                  </Carousel.Item>
+                  <Carousel.Item className="fuul__width">
+                    <Row>
+                      <Image src={Slide1} alt="banner immage " className="full_width" />
+                    </Row>
+                  </Carousel.Item>
+                  <Carousel.Item className="fuul__width">
+                    <Row>
+                      <Image src={Slide1} alt="banner immage" className="full_width" />
+                    </Row>
+                  </Carousel.Item>
+                </Carousel>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+      <div className="parent1313">
+        <div className="test1313">
+          <Container className="container-borderer">
+            <Row>
+              <Col xs="3">
+                <Link href="/ProjectsFloor">
+                  <div className="mb-5 projects--title cursor">
+                    <p>Done</p>
+                    <h3>
+                      OLD CITY <br /> PANORAMA
+                    </h3>
+                  </div>
+                </Link>
+                <div className="projects--legend__top projects--texts__basics">
+                  <p className="projects--texts__op">Location</p>
+                  <h3 className="project--location__mod projects--texts__basics">
+                    Tbilisi,georgia
+                    <br />
+                    bukhaidze 6a
+                  </h3>
+                </div>
+                <div className="projects--legend__bot">
+                  <p className="projects--texts__op projects--texts__create">Create date</p>
+                  <h3 className="projects--texts__basics projects--texts__done ">2022</h3>
+                </div>
+                <div>
+                  <Link href="/ProjectsFloor">
+                    <i className="project--see cursor">
+                      View Projects <Image src={Vector} />
+                    </i>
+                  </Link>
+                </div>
+              </Col>
+              <Col xs="9" className="projects-margin-fixer">
+                <Carousel style={{ opacity: 1 }}>
+                  <Carousel.Item className="fuul__width">
+                    <Image src={Slide1} alt="banner immage" className="full_width" />
+                  </Carousel.Item>
+                  <Carousel.Item className="fuul__width">
+                    <Row>
+                      <Image src={Slide1} alt="banner immage " className="full_width" />
+                    </Row>
+                  </Carousel.Item>
+                  <Carousel.Item className="fuul__width">
+                    <Row>
+                      <Image src={Slide1} alt="banner immage" className="full_width" />
+                    </Row>
+                  </Carousel.Item>
+                </Carousel>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
     </Layout>
   );
 };
