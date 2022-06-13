feb579905e249ce70be8b4138d638aaca1c29717
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-06-13 14:26:29 +0400

    images update

--- src/components/about/AboutBanner.jsx
+++ src/components/about/AboutBanner.jsx
@@ -4,6 +4,27 @@ import BannerImg from "../../../assets/aboutbannerimg.svg";
 import Beqa from "../../../assets/beqa.svg";
 import Beqafull from "../../../assets/beqafull.svg";
 import Image from "next/image";
+import Team1 from "../../../assets/team/team1.jpg";
+import Team2 from "../../../assets/team/team2.jpg";
+import Team3 from "../../../assets/team/team3.jpg";
+// import Team4 from "../../../assets/team/team4.jpg";
+import Team5 from "../../../assets/team/team5.jpg";
+import Team6 from "../../../assets/team/team6.jpg";
+import Team7 from "../../../assets/team/team7.jpg";
+import Team8 from "../../../assets/team/team8.jpg";
+import Team9 from "../../../assets/team/team9.jpg";
+import Team10 from "../../../assets/team/team10.jpg";
+import Team11 from "../../../assets/team/team11.jpg";
+import Team12 from "../../../assets/team/team12.jpg";
+import Team13 from "../../../assets/team/team13.jpg";
+import Team14 from "../../../assets/team/team14.jpg";
+import Team15 from "../../../assets/team/team15.jpg";
+import Team16 from "../../../assets/team/team16.jpg";
+import Team17 from "../../../assets/team/team17.jpg";
+import Team18 from "../../../assets/team/team18.jpg";
+import Team19 from "../../../assets/team/team19.jpg";
+import Team20 from "../../../assets/team/team20.jpg";
+import Team21 from "../../../assets/team/team21.jpg";
 import Quote from "../../../assets/quote.svg";
 import Video from "../../../assets/video.svg";
 export const AboutBanner = () => {
@@ -79,68 +100,68 @@ export const AboutBanner = () => {
         </Col>
       </Container>
       <section className="cover mt-120"></section>
-      <Container className="mb-120 mt-120" >
+      <Container className="mb-120 mt-120">
         <Row className="row-marginer mb-60 ">
           <h1>OUR TEAM</h1>
         </Row>
         <Row className="sixty-row">
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team1} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team2} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team3} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team3} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team5} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team6} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team7} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team8} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team9} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team10} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team11} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
           <Col xs="3">
-            <Image src={Beqafull} alt="banner immage" />
+            <Image src={Team12} alt="banner immage" />
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
