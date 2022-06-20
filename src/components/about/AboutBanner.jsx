bd462f8ab24008f50e52a23d4ecf494faa326254
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-06-21 01:03:52 +0400

    about

--- src/components/about/AboutBanner.jsx
+++ src/components/about/AboutBanner.jsx
@@ -31,7 +31,7 @@ export const AboutBanner = () => {
   return (
     <div>
       <Container className="mt-5">
-        <h1 className="about-title">ABOUT US</h1>
+        <h1 className="about-title ">ABOUT US</h1>
         <Row className="row-marginer justify-content-end">
           <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3" className="d-flex justify-content-center">
             <Image src={Beqa} alt="banner immage" />
@@ -52,58 +52,66 @@ export const AboutBanner = () => {
           </Col>
         </Row>
         <Row className="justify-content-between mb-60 mt-60">
-          <Col  xs="5" lg="3" md="3" sm="3" xl="3" xxl="3">
+          <Col xs="5" lg="3" md="3" sm="3" xl="3" xxl="3">
             <hr />
           </Col>
-          <Col  xs="5" lg="3" md="3" sm="3" xl="3" xxl="3">
+          <Col xs="5" lg="3" md="3" sm="3" xl="3" xxl="3">
             <hr />
           </Col>
         </Row>
         <Row className="mb-60 mt-60 ">
-          <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" >
-          <Row >
-          <Col xs="12" lg="12" md="12" sm="12" xl="12" xxl="12">
-            <h3 className="about--text__hmod">In 2020 Apart Development</h3>
-          </Col>
-          <Col xs="12" lg="12" md="12" sm="12" xl="12" xxl="12">
-            <p className="about--text__ptmod2">
-              started new project OLD CITY PANORAMA whitch is different and special poject in Tbilisi. Project
-              perfectly suits the architectural style of Old Tbilisi, It is located on Bukhaidze Street, in
-              the historical part of Tbilisi. Our goal is to match the exterior of the project perfectly with
-              the wider city planning, which is why the Old City Panorama project includes three buildings and
-              is only eight floors high. The project is also important because it has many sights close to it:
-              one of the most important attractions of the city, the Holy Trinity Cathedral, just 250 meters
-              away, the historic Metekhi Fortress and Temple, and Narikala and Rike Park.
-            </p>
-          </Col>
-          </Row>
+          <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
+            <Row>
+              <Col xs="12" lg="12" md="12" sm="12" xl="12" xxl="12">
+                <h3 className="about--text__hmod">In 2020 Apart Development</h3>
+              </Col>
+              <Col xs="12" lg="12" md="12" sm="12" xl="12" xxl="12">
+                <p className="about--text__ptmod2">
+                  started new project OLD CITY PANORAMA whitch is different and special poject in Tbilisi.
+                  Project perfectly suits the architectural style of Old Tbilisi, It is located on Bukhaidze
+                  Street, in the historical part of Tbilisi. Our goal is to match the exterior of the project
+                  perfectly with the wider city planning, which is why the Old City Panorama project includes
+                  three buildings and is only eight floors high. The project is also important because it has
+                  many sights close to it: one of the most important attractions of the city, the Holy Trinity
+                  Cathedral, just 250 meters away, the historic Metekhi Fortress and Temple, and Narikala and
+                  Rike Park.
+                </p>
+              </Col>
+            </Row>
           </Col>
           <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-          <Row>
-        <Col xs="12" lg="12" md="12" sm="12" xl="12" xxl="12" className="mt-60 ">
-            <h3 className="about--text__hmod">Another advantage</h3>
-          </Col>
-          <Col xs="12" lg="12" md="12" sm="12" xl="12" xxl="12">
-            <p className="about--text__ptmod__mod ">
-              OLD CITY PANORAMA is the amazing views and greenery of Old Tbilisi. For residents on the first
-              floor, living in “townhouse” type dwellings, we have individual yards. The area includes a
-              swimming pool, spa, gym equipment and a cafe-bar. The apartments are being delivered both in
-              green frame condition and with complete renovation. Buying an apartment with Apart Development
-              is a sensible investment, as we also offer rental service to our customers if they wish, which
-              means that the apartment owner can turn their real estate into a source of income without a lot
-              of effort. Our goal is to continue to strive for progress, to contribute to the urban
-              development of Tbilisi, which means that we want to cover the territories of Old Tbilisi,
-              replace damaged buildings with new residential complexes, which will be the best example of
-              combining the Old Tbilisi architectural style with the new. We want to give new life to Old
-              Tbilisi and create a service focused on the wishes and needs of customers. Apart Development
-              will establish a new lifestyle in Old Tbilisi.
-            </p>
+            <Row>
+              <Col
+                xs="12"
+                lg="12"
+                md="12"
+                sm="12"
+                xl="12"
+                xxl="12"
+                // className="mt-60 "
+              >
+                <h3 className="about--text__hmod">Another advantage</h3>
+              </Col>
+              <Col xs="12" lg="12" md="12" sm="12" xl="12" xxl="12">
+                <p className="about--text__ptmod__mod ">
+                  OLD CITY PANORAMA is the amazing views and greenery of Old Tbilisi. For residents on the
+                  first floor, living in “townhouse” type dwellings, we have individual yards. The area
+                  includes a swimming pool, spa, gym equipment and a cafe-bar. The apartments are being
+                  delivered both in green frame condition and with complete renovation. Buying an apartment
+                  with Apart Development is a sensible investment, as we also offer rental service to our
+                  customers if they wish, which means that the apartment owner can turn their real estate into
+                  a source of income without a lot of effort. Our goal is to continue to strive for progress,
+                  to contribute to the urban development of Tbilisi, which means that we want to cover the
+                  territories of Old Tbilisi, replace damaged buildings with new residential complexes, which
+                  will be the best example of combining the Old Tbilisi architectural style with the new. We
+                  want to give new life to Old Tbilisi and create a service focused on the wishes and needs of
+                  customers. Apart Development will establish a new lifestyle in Old Tbilisi.
+                </p>
+              </Col>
+            </Row>
           </Col>
         </Row>
-            </Col>
-        </Row>
-       
-      
+
         <Col xs="12" className="mb-60 mt-60">
           <Image src={Video} alt="banner immage" />
         </Col>
@@ -119,6 +127,7 @@ export const AboutBanner = () => {
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>
+
           {/* <Col xs="6" lg="3" md="3" sm="3" xl="3" xxl="3">
             <Image src={Team2} alt="banner immage"/>
             <h3>Beka Khoperia</h3>
@@ -229,7 +238,7 @@ export const AboutBanner = () => {
             <h3>Beka Khoperia</h3>
             <p>Founder</p>
           </Col>*/}
-        </Row> 
+        </Row>
       </Container>
     </div>
   );
