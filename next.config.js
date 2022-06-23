f4ceb7944549d27c224dcecacaebc93469a729fa
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-06-23 21:37:01 +0400

    test360

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  i18n: {
+    // providing the locales supported by your application
+    locales: ["en-US", "es-ES", "it-IT"],
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "en-US"
+  }
+};
+
---
+++ src/components/Slider/ImgView/data.js
@@ -0,0 +1,19 @@
+export default [
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-01.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-02.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-03.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-04.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-05.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-06.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-07.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-08.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-09.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-10.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-11.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-12.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-13.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-14.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-15.jpg?impolicy=myresize&rw=1600",
+    "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2020/2020-street-750/2020-street-750-e87/360/2020-street-750-e87-motorcycle-16.jpg?impolicy=myresize&rw=1600"
+  ];
+  
