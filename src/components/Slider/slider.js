6b4457c601460487d19041e265029c1118875785
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-06-24 01:40:57 +0400

    123

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -1,71 +1,149 @@
-import { useState } from "react";
-import Slider from "react-slick";
-// import Test from "../Test/Test";
-// import D3scene from "../3dscene/3dscene";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import React from 'react'
+import Flickity from "react-flickity-component";
 import { Row, Col,Container } from "react-bootstrap";
 import Slide1 from "../../../assets/slider.png";
 import Slide2 from "../../../assets/slider.png";
 import Slide3 from "../../../assets/slider.png";
 import Slide4 from "../../../assets/slider.png";
 import Taghi from "../../../assets/taghi.svg";
-
+import Call from "../../../assets/Call.svg";
+import ThreeSixty from "../../../assets/ThreeSixtt.svg"
+import Video from "../../../assets/Videosmall.svg"
+import Vector from "../../../assets/seeprjvector.png"
 import Image from "next/image";
-
-const images = [Slide1, Slide2, Slide3, Slide4];
-function Dslider() {
-  const NextArrow = ({ onClick }) => {
-    return (
-      <div className="arrow next" onClick={onClick}>
-        <FaArrowRight />
-      </div>
-    );
-  };
-
-  const PrevArrow = ({ onClick }) => {
-    return (
-      <div className="arrow prev" onClick={onClick}>
-        <FaArrowLeft />
-      </div>
-    );
-  };
-
-  const [imageIndex, setImageIndex] = useState(0);
-
-  const settings = {
-    infinite: true,
-    lazyLoad: true,
-    speed: 500,
-    slidesToShow: 3,
-    centerMode: true,
-    centerPadding: 0,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    beforeChange: (current, next) => setImageIndex(next),
-  };
-
+const Slider = () => {
   return (
-    <div className="custom_row">
-      <div className="App">
-        <Slider {...settings}>
-          {images.map((img, idx) => (
-            <div
-              style={{ width: 100 }}
-              key={idx}
-              className={idx === imageIndex ? "slide activeSlide" : "slide"}
-            >
-              <Container>
+    <div>
+    <Flickity  data-flickity='{ "freeScroll": true, "wrapAround": true }'>
+    <Container>
               <Row>
                 <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" >
-                 <Image src={img} alt={img} />
-                   {/* <iframe
+                <iframe
                     allowFullScreen
                     width="640"
                     height="480"
                     loading="lazy"
                     frameBorder="0"
-                    src="https://p3d.in/e/M6xbJ+load+nopan+nozoom+controls,border,loader-hidden"
-                  ></iframe>  */}
+                    src="https://62b4daba41b2130008b80a32--resilient-ganache-549f6c.netlify.app/frame/"
+                  ></iframe>
+             
+                </Col>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" className="d-flex">
+                  <Col className="d-flex align-items-center">
+                    <div className="test">
+
+              
+                     <Image src={Taghi} alt="banner immage" className="Taghi" /> 
+                      <p className="mt-40">
+                        For our team, it is important and a priority to preserve <br />
+                        the charm of the old city, to use the old
+                        <br />
+                        <br />
+                        For our team, it is important and a priority to preserve <br /> the charm of the old
+                        city, to use the old
+                      </p>
+                      <Row  className="mt-40">
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={ThreeSixty} />
+                          </Col>
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={Call} />
+                          </Col>
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={Video} />
+                          </Col>
+                          <Col  xs="6" lg="6" md="6" sm="6" xl="6" xxl="6">
+                          <i className="read__more cursor">
+                              View Projects <Image src={Vector} />
+                            </i>
+                          </Col>
+                        </Row>
+                    </div>
+                    
+                  </Col>
+                </Col>
+              </Row>
+      </Container>
+      <Container>
+              <Row>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" >
+                 <Image src={Slide1} />
+                </Col>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" className="d-flex">
+                  <Col className="d-flex align-items-center">
+                    <div className="test">
+                      <Image src={Taghi} alt="banner immage" className="Taghi" />
+                      <p className="mt-40">
+                        For our team, it is important and a priority to preserve <br />
+                        the charm of the old city, to use the old
+                        <br />
+                        <br />
+                        For our team, it is important and a priority to preserve <br /> the charm of the old
+                        city, to use the old
+                      </p>
+                      <Row  className="mt-40">
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={ThreeSixty} />
+                          </Col>
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={Call} />
+                          </Col>
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={Video} />
+                          </Col>
+                          <Col  xs="6" lg="6" md="6" sm="6" xl="6" xxl="6">
+                          <i className="read__more cursor">
+                              View Projects <Image src={Vector} />
+                            </i>
+                          </Col>
+                        </Row>
+                    </div>
+                  </Col>
+                </Col>
+              </Row>
+      </Container>
+      <Container>
+              <Row>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" >
+                 <Image src={Slide1} />
+                </Col>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" className="d-flex">
+                  <Col className="d-flex align-items-center">
+                    <div className="test">
+                      <Image src={Taghi} alt="banner immage" className="Taghi" />
+                      <p className="mt-40">
+                        For our team, it is important and a priority to preserve <br />
+                        the charm of the old city, to use the old
+                        <br />
+                        <br />
+                        For our team, it is important and a priority to preserve <br /> the charm of the old
+                        city, to use the old
+                      </p>
+                      <Row  className="mt-40">
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={ThreeSixty} />
+                          </Col>
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={Call} />
+                          </Col>
+                          <Col xs="2" lg="2" md="2" sm="2" xl="2" xxl="2">
+                          <Image src={Video} />
+                          </Col>
+                          <Col  xs="6" lg="6" md="6" sm="6" xl="6" xxl="6">
+                          <i className="read__more cursor">
+                              View Projects <Image src={Vector} />
+                            </i>
+                          </Col>
+                        </Row>
+                    </div>
+                  </Col>
+                </Col>
+              </Row>
+      </Container>
+      <Container>
+              <Row>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" >
+                 <Image src={Slide1} />
                 </Col>
                 <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" className="d-flex">
                   <Col className="d-flex align-items-center">
@@ -83,12 +161,33 @@ function Dslider() {
                   </Col>
                 </Col>
               </Row>
-              </Container>
-            </div>
-          ))}
-        </Slider>
-      </div>
+      </Container>
+      <Container>
+              <Row>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" >
+                 <Image src={Slide1} />
+                </Col>
+                <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6" className="d-flex">
+                  <Col className="d-flex align-items-center">
+                    <div className="test">
+                      <Image src={Taghi} alt="banner immage" className="Taghi" />
+                      <p className="mt-40">
+                        For our team, it is important and a priority to preserve <br />
+                        the charm of the old city, to use the old
+                        <br />
+                        <br />
+                        For our team, it is important and a priority to preserve <br /> the charm of the old
+                        city, to use the old
+                      </p>
+                    </div>
+                  </Col>
+                </Col>
+              </Row>
+      </Container>
+    </Flickity>
     </div>
-  );
+    
+  )
 }
-export default Dslider;
+
+export default Slider
