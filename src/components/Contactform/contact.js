e3aef70b25485ffb80b3b1ad4012b3a4c3089d2c
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-06-24 01:11:09 +0400

    home updates styles

--- src/components/Contactform/contact.js
+++ src/components/Contactform/contact.js
@@ -1,104 +1,108 @@
-import React from 'react'
-import { Nav, Button,Row,Container,input,Col,Form } from 'react-bootstrap'
+import React from "react";
+import { Nav, Button, Row, Container, input, Col, Form } from "react-bootstrap";
 function contact() {
-return (
-<Container className='container-marginer additional__padding'>
-   <Row className="justify-content-between" >
-      <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-         <Row>
-      <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3">
-         <p>
-            About us
-         </p>
-      </Col>
-      <Col xs="12" lg="9" md="9" sm="9" xl="9" xxl="9">
-         <h2 className='section1-title'>
-         Renewed life of old tbilisi
-         </h2>
-         <p className='section1-subtitle'>
-            For our team, it is important and a priority to preserve the charm of the old city, 
-            to use the old architectural fragments in the modern construction, 
-            and at the same time to bring new life to the forgotten neighborhoods.
-         </p>
-      </Col>
+  return (
+    <Container className="container-marginer additional__padding">
+      <Row className="justify-content-between">
+        <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
+          <Row>
+            <Col xs="12" lg="3" md="3" sm="3" xl="3" xxl="3">
+              <p className="home--section__texts">About us</p>
+            </Col>
+            <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
+              <h1 className="section1-title">RENEWED LIFE OF OLD TBILISI</h1>
+            </Col>
+            <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
+              <p className="section1-subtitle">
+                For our team, it is important and a priority to preserve the charm of the old city, to use the
+                old architectural fragments in the modern construction, and at the same time to bring new life
+                to the forgotten neighborhoods.
+              </p>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
+          <Col xs="12" className="col-5 padding-0">
+            <p className="section1-form-text">
+              Leave us your contact information and our team will reach out to you.
+            </p>
+            <h4 className="section-1-form-subtext">Apartment type:</h4>
+            <Row>
+              <Col xs="3">
+                <Form>
+                  {["checkbox"].map(type => (
+                    <div key={`inline-${type}`} className="mb-3">
+                      <Form.Check inline label="Studio" name="group1" type={type} id={`inline-${type}-1`} />
+                    </div>
+                  ))}
+                </Form>
+              </Col>
+              <Col xs="3">
+                <Form>
+                  {["checkbox"].map(type => (
+                    <div key={`inline-${type}`} className="mb-3">
+                      <Form.Check
+                        inline
+                        label="1 Bedroom"
+                        name="group1"
+                        type={type}
+                        id={`inline-${type}-2`}
+                      />
+                    </div>
+                  ))}
+                </Form>
+              </Col>
+              <Col xs="6" className="col-6">
+                <Form>
+                  {["checkbox"].map(type => (
+                    <div key={`inline-${type}`} className="mb-3">
+                      <Form.Check
+                        inline
+                        label="2 and more bedrooms"
+                        name="group1"
+                        type={type}
+                        id={`inline-${type}-2`}
+                      />
+                    </div>
+                  ))}
+                </Form>
+              </Col>
+              <Col className="mb-60">
+                <div xs="12" className="mb-md-0 mb-5">
+                  <Form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                    <Col xs="12">
+                      <div className="md-form mb-0">
+                        <Form.Control
+                          type="text"
+                          id="name"
+                          name="name"
+                          className="form-control input__main"
+                          placeholder="Phone"
+                        />
+                      </div>
+                    </Col>
+                    <Col xs="12">
+                      <div className="md-form mb-0">
+                        <Form.Control
+                          type="text"
+                          id="email"
+                          name="email"
+                          className="form-control input__main"
+                          placeholder="Email"
+                        />
+                      </div>
+                    </Col>
+                  </Form>
+                  <div className="text-center text-md-left">
+                    <Button className="btn-send">Get contacted</Button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Col>
       </Row>
-      </Col>
-      <Col xs="12" lg="6" md="6" sm="6" xl="6" xxl="6">
-      <Col xs="12" className='col-5 padding-0'>
-         <p className='section1-form-text'>
-            Leave us your contact information and our team will reach out to you.
-         </p>
-         <h4 className='section-1-form-subtext'>Apartment type:</h4>
-         <Row>
-            <Col xs="3">
-               <Form>
-               {['checkbox'].map((type) => (
-                  <div key={`inline-${type}`} className="mb-3">
-                     <Form.Check
-                     inline
-                     label="Studio"
-                     name="group1"
-                     type={type}
-                     id={`inline-${type}-1`}
-                     />
-                  </div>
-               ))}
-               </Form>
-            </Col>
-            <Col xs="3">
-               <Form>
-               {['checkbox'].map((type) => (
-                  <div key={`inline-${type}`} className="mb-3">
-                     <Form.Check
-                     inline
-                     label="1 Bedroom"
-                     name="group1"
-                     type={type}
-                     id={`inline-${type}-2`}
-                     />
-                  </div>
-               ))}
-               </Form>
-            </Col>
-            <Col xs="6" className='col-6'>
-            <Form>
-               {['checkbox'].map((type) => (
-                  <div key={`inline-${type}`} className="mb-3">
-                     <Form.Check
-                     inline
-                     label="2 and more bedrooms"
-                     name="group1"
-                     type={type}
-                     id={`inline-${type}-2`}
-                     />
-                  </div>
-               ))}
-               </Form>
-            </Col>
-               <Col className="mb-60">
-                  <div xs="12" className="mb-md-0 mb-5">
-                     <Form id="contact-form" name="contact-form" action="mail.php" method="POST">      
-                           <Col xs="12">
-                              <div className="md-form mb-0">
-                                 <Form.Control type="text" id="name" name="name" className="form-control input__main" placeholder='Phone'/>
-                              </div>
-                           </Col>
-                           <Col xs="12" >
-                              <div className="md-form mb-0">
-                                 <Form.Control type="text" id="email" name="email" className="form-control input__main" placeholder='Email'/>
-                              </div>
-                           </Col>
-                     </Form>
-                     <div className="text-center text-md-left">
-                        <Button className="btn-send">Get contacted</Button>
-                     </div>
-                  </div>
-               </Col>
-         </Row>
-      </Col>
-      </Col>
-   </Row>
-</Container>
-)
+    </Container>
+  );
 }
-export default contact
+export default contact;
--- src/components/Partners/partners.js
+++ src/components/Partners/partners.js
@@ -1,45 +1,43 @@
-import React from 'react'
-import { Nav, Button,Row,Container,input, Col } from 'react-bootstrap'
-import Apart_showroom from '../../../assets/apartshowroom.svg'
-import Greenlife from '../../../assets/greenlife.svg'
-import Malongo from '../../../assets/malongo.svg'
-import Tbc from '../../../assets/tbc.svg'
+import React from "react";
+import { Nav, Button, Row, Container, input, Col } from "react-bootstrap";
+import Apart_showroom from "../../../assets/apartshowroom.svg";
+import Greenlife from "../../../assets/greenlife.svg";
+import Malongo from "../../../assets/malongo.svg";
+import Tbc from "../../../assets/tbc.svg";
 
-import Image from 'next/image'
+import Image from "next/image";
 function Partners() {
   return (
-        <Container className='partners-container mb-60 mt-60'>
-            <Row className="justify-content-between align-items-center">
-              <Col xs="12" lg="2" md="2" sm="2" xl="2" xxl="2" >
-                <div className='partners'>
-                  <p>
-                  Sponsors
-                  </p>
-                  </div>
-              </Col>
-                <div className='col-2 '>
-                  <div className='partners'>
-                  <Image src={Apart_showroom} alt="banner immage" className='partners_wite' />
-                  </div>  
-                </div>
-                <div className='col-2'>
-                {/* <div className='partners'>
+    <Container className="partners-container mb-60 mt-60">
+      <Row className="justify-content-between align-items-center">
+        <Col xs="12" lg="2" md="2" sm="2" xl="2" xxl="2">
+          <div className="partners home--section__texts">
+            <p>Sponsors</p>
+          </div>
+        </Col>
+        <div className="col-2 ">
+          <div className="partners">
+            <Image src={Apart_showroom} alt="banner immage" className="partners_wite" />
+          </div>
+        </div>
+        <div className="col-2">
+          {/* <div className='partners'>
                 <Image src={Greenlife} alt="banner immage" className='partners_wite'/>
                   </div>   */}
-                </div>
-                <div className='col-2'>
-                <div className='partners'>
-                <Image src={Malongo} alt="banner immage" className='partners_wite'/>
-                  </div>  
-                </div>
-                <div className='col-2'>
-                <div className='partners'>
-                <Image src={Tbc} alt="banner immage" className='partners_wite'/>
-                  </div>  
-                </div>
-            </Row>
-        </Container>
-  )
+        </div>
+        <div className="col-2">
+          <div className="partners">
+            <Image src={Malongo} alt="banner immage" className="partners_wite" />
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="partners">
+            <Image src={Tbc} alt="banner immage" className="partners_wite" />
+          </div>
+        </div>
+      </Row>
+    </Container>
+  );
 }
 
-export default Partners
+export default Partners;
